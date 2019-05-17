ccc0950718880dbfa9692d9f2f9d33fcda6b5c59
zetaemme <mattia.zorzan@gmail.com>
2019-05-17 16:14:18 +0200

    Initial commit

---
+++ clientReq-server/src/clientReq.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    printf("Hi, I'm ClientReq program!\n");
+    return 0;
+}
---
+++ clientReq-server/src/server.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[]) {
+    printf("Hi, I'm Server program!\n");
+    return 0;
+}
